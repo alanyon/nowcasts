3facb4b3f2fce7614efe2c007794c95d27fbd72e
Lanyon, Andre <andre.lanyon@metoffice.gov.uk>
2022-11-22 09:35:06 +0000

    Adding code

--- run_nowcast.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-#SBATCH --qos=normal
-#SBATCH --mem=20G
-#SBATCH --ntasks=4
-#SBATCH --output=/home/h04/alanyon/HAIC/test.out
-#SBATCH --time=30
-#SBATCH --error=/home/h04/alanyon/HAIC/test.err
